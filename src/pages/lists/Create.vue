77ac9b00d85f84c8b60b5c5230e50c5820fbaf55
João <joaohccl@hotmail.com>
2020-06-08 16:31:41 +0900

    updated created page and firebase requests to store lists

--- src/pages/lists/Create.vue
+++ src/pages/lists/Create.vue
@@ -1,5 +1,5 @@
 <template>
-    <f7-popup id="creation-page">
+    <f7-popup id="creation-page" ref="cPopUp">
         <f7-page>
             <f7-navbar>
                 <f7-nav-title>
@@ -14,11 +14,11 @@
             <f7-list form>
                 <f7-list-item>
                     <f7-label>Name of your list</f7-label>
-                    <f7-input input-with-value type="text" placeholder="My todo list Name" required></f7-input>
+                    <f7-input name="list-name" type="text" placeholder="My todo list Name" required></f7-input>
                 </f7-list-item>
                 <f7-list-item>
                     <f7-label>Description</f7-label>
-                    <f7-input type="textarea" placeholder="More information you wish to specify about the list" resizable></f7-input>
+                    <f7-input name="description" type="textarea" placeholder="More information you wish to specify about the list" resizable></f7-input>
                 </f7-list-item>
                 <input type="file" ref="file" style="display: none" accept="image/*" @change="getPreviewImage">
                 <f7-list-button @click="takePhoto()">
@@ -40,13 +40,16 @@
 
     export default {
         name: "Create",
+
         data () {
             return {
                 previewImg: '',
                 imgFile: null
             }
         },
+
         methods: {
+
             takePhoto () {
                 let app = new Framework7();
                 // TODO this still needs testing but the online thingy isnt building up the project properly
@@ -67,47 +70,61 @@
                     this.$refs.file.click();
                 }
             },
+
             getPreviewImage(e) {
                 this.imgFile = e.target.files[0];
                 this.previewImg = URL.createObjectURL(this.imgFile);
             },
+
+            getFileExtension(filename) {
+                // the + 1 is to remove the dot. in this case, it could be omitted but it is added after to be more clear
+                return filename.slice(filename.lastIndexOf(".") + 1, filename.length);
+            },
+
+            updateDatabase(downloadURL) {
+                console.log('File available at', downloadURL);
+                let name = this.$$('#creation-page .input-with-value[name="list-name"]').val();
+                let description = this.$$('#creation-page .input-with-value[name="description"]').val();
+                let date = new Date();
+                let self = this;
+
+                fb.db.collection("todos").add({
+                    name,
+                    description,
+                    date,
+                    imgURL: downloadURL,
+                    uid: fb.auth.currentUser.uid
+                })
+                    .then(function(docRef) {
+                        console.log("Successfully added the picture at " + docRef.id);
+                        self.$refs.cPopUp.close();
+                    })
+                    .catch(function(error) {
+                        console.log("Error at updating database " + error.toString())
+                    });
+            },
+
             submitForm() {
                 if (!this.previewImg) {
                     return false
                 }
+                let self = this;
                 let ref = fb.storage.ref();
+                // one way of making the file name unique is to use current time
+                let fileName = `${Date.now()}.${this.getFileExtension(this.imgFile.name)}`;
 
-                let uploadTask = ref.child('images/' + this.imgFile.name).put(this.imgFile);
+                let uploadTask = ref.child(`images/${fb.auth.currentUser.uid}/${fileName}`).put(this.imgFile);
                 // Listen for state changes, errors, and completion of the upload.
                 uploadTask.on(firebase.storage.TaskEvent.STATE_CHANGED, // or 'state_changed'
                     function(snapshot) {
                         // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
-                        let progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-                        console.log('Upload is ' + progress + '% done');
-                        switch (snapshot.state) {
-                            case firebase.storage.TaskState.PAUSED: // or 'paused'
-                                console.log('Upload is paused');
-                                break;
-                            case firebase.storage.TaskState.RUNNING: // or 'running'
-                                console.log('Upload is running');
-                                break;
-                        }
+                        // this can be useful to do loading sections or anything
                     }, function(error) {
                         console.log("Error at uploading picture " + error.code)
                     }, function() {
-                        // Upload completed successfully, now we can get the download URL
-                        uploadTask.snapshot.ref.getDownloadURL().then(function(downloadURL) {
-                            console.log('File available at', downloadURL);
-                            fb.db.collection("teste").add({
-                                coiso: downloadURL
-                            })
-                                .then(function(docRef) {
-                                    console.log("Successfully added the picture at " + docRef.id)
-                                })
-                                .catch(function(error) {
-                                    console.log("Error at updating database " + error.toString())
-                                });
-                        });
+
+                        // Upload completed successfully, now we can get the download URL and update the database
+                        uploadTask.snapshot.ref.getDownloadURL().then(self.updateDatabase);
                     });
             }
         }
