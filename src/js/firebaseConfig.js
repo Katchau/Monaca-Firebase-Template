c4aaf220e15da31a4615ea53c8eab9cddaa61bca
Jonas L <joaohccl@hotmail.com>
2020-07-07 20:56:54 +0900

    removing stuff for the template

--- src/js/firebaseConfig.js
+++ src/js/firebaseConfig.js
@@ -6,14 +6,14 @@ import 'firebase/storage'
 
 // firebase init goes here
 const firebaseConfig = {
-    apiKey: "AIzaSyDi12gl0FBff9JihCAekYeaH-SwxyEQFec",
-    authDomain: "fir-m-7abf4.firebaseapp.com",
-    databaseURL: "https://fir-m-7abf4.firebaseio.com",
-    projectId: "fir-m-7abf4",
-    storageBucket: "fir-m-7abf4.appspot.com",
-    messagingSenderId: "483137997326",
-    appId: "1:483137997326:web:8af4ccbaf140dcf0bbca5f",
-    measurementId: "G-GLFZMQ67D0"
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
 };
 firebase.initializeApp(firebaseConfig);
 
@@ -25,8 +25,8 @@ const auth = firebase.auth();
 // These are optional parameters you can export to make it faster to access later on the app
 // const currentUser = auth.currentUser
 // firebase collections
-// const usersCollection = db.collection('users')
-// const postsCollection = db.collection('posts')
+// const usersCollection = db.collection('todos')
+// you can add other things depending on what you are doing
 
 export {
     auth,
